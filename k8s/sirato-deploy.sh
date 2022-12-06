188a64bd6d0046b1d05375247ce3db3f34f93f41
Conor Svensson <conor10@gmail.com>
2022-12-06 19:36:11 +0100

    K8s support (#53)
    * Add k8s deployment support

---
+++ k8s/sirato-deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+endpoint=""
+
+if [ $# -eq 1 ]; then
+  echo "Using endpoint: $1"
+  endpoint=$1
+else
+  echo "Please specify a node endpoint"
+  exit 1
+fi
+
+kubectl create namespace vmbc-epirus
+NAMESPACE="vmbc-epirus"
+
+cp epirus-net-networkpolicy.yml.tmpl epirus-net-networkpolicy.yml
+cp api-deployment.yml.tmpl api-deployment.yml
+cp ingestion-deployment.yml.tmpl ingestion-deployment.yml
+cp mongodb-deployment.yml.tmpl mongodb-deployment.yml
+cp web-deployment.yml.tmpl web-deployment.yml
+
+kubectl apply -f epirus-net-networkpolicy.yml,mongodb-deployment.yml,web-deployment.yml
+echo fetching mongodb IP.....
+sleep 60
+name=$(kubectl get pods | grep mongo | awk -F '1/1' '{print $1}')
+ip=$(kubectl describe pod $name | awk -F 'IP:               ' '{print $2}')
+echo $name
+echo $ip
+latest=$(echo $ip)
+sed -i "s|IP|$latest|g" "ingestion-deployment.yml"
+sed -i "s|IP|$latest|g" "api-deployment.yml"
+
+sed -i "s|NODEENDPOINT|$endpoint|g" "ingestion-deployment.yml"
+sed -i "s|NODEENDPOINT|$endpoint|g" "api-deployment.yml"
+
+kubectl apply -f api-deployment.yml,ingestion-deployment.yml
