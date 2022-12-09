903e0d3f3da975784ba615d986d9c0696c6acf75
mohamedelshami <1651925+mohamedelshami@users.noreply.github.com>
2022-12-09 22:11:14 +0200

    Added OS switch for sed command

--- k8s/sirato-launch.sh
+++ k8s/sirato-launch.sh
@@ -21,6 +21,12 @@ cp mongodb-deployment.yml.tmpl mongodb-deployment.yml
 cp web-deployment.yml.tmpl web-deployment.yml
 cp proxy.yml.tmpl proxy.yml
 
-sed -i "s|NODEENDPOINT|$endpoint|g" "ingestion-deployment.yml"
-sed -i "s|NODEENDPOINT|$endpoint|g" "api-deployment.yml"
+if [ "$ARCH" == "Darwin" ]; then
+  sed -i '' "s|NODEENDPOINT|$endpoint|g" "ingestion-deployment.yml"
+  sed -i '' "s|NODEENDPOINT|$endpoint|g" "api-deployment.yml"
+else
+  sed -i "s|NODEENDPOINT|$endpoint|g" "ingestion-deployment.yml"
+  sed -i "s|NODEENDPOINT|$endpoint|g" "api-deployment.yml"
+fi
+
 kubectl create -f mongodb-deployment.yml,api-deployment.yml,ingestion-deployment.yml,web-deployment.yml,proxy.yml
