1e14c4b644c00fe52e6a42265ab7d40207a78e2a
Marc Fornos <frozen.pomelo@gmail.com>
2022-12-12 12:35:46 +0100

    refine k8s

--- k8s/sirato-destroy.sh
+++ k8s/sirato-destroy.sh
@@ -1,24 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -eu
 
-echo ---------------- Deleting Epirus-mogodb PoD and Service ----------------
-kubectl delete -f mongodb-deployment.yml
-
-echo ---------------- Deleting Epirus-web PoD and Service ----------------
-kubectl delete -f web-deployment.yml
-
-echo ---------------- Deleting Epirus-api PoD and Service ----------------
-kubectl delete -f api-deployment.yml
-
-echo ---------------- Deleting Epirus-ingestion PoD ----------------
-kubectl delete -f ingestion-deployment.yml
-
-echo ---------------- Deleting Proxy PoD ----------------
-kubectl delete -f proxy.yml
-
-echo ---------------- Deleting vmbc-sirato Namespace ----------------
+kubectl delete --all deployments -n sirato-explorer
+kubectl delete --all services -n sirato-explorer
 kubectl delete namespace sirato-explorer
-
-echo ---------------- Removing tmp files ----------------
-rm -rvf mongodb-deployment.yml web-deployment.yml api-deployment.yml ingestion-deployment.yml proxy.yml 
-
-echo ========================== DONE ===========================
